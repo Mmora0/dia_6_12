b3b06fb733e41eaf4ab25806a93e30e78176a41e
Mmora0 <mamora171@misena.edu.co>
2023-05-29 19:24:00 -0500

    se inicia el proyecto

---
+++ java.js
@@ -0,0 +1,6 @@
+var nombre = prompt ('Bienvenido, identificate. Escribe tu nombre: ')
+alert('Hola ' + nombre + ', te contaré un chiste.')
+alert('Que le dice un pollito a otro pollito?')
+alert('Necesitamos a Pollo')
+
+document.write("Hola " + nombre + ", bienvenido.")
